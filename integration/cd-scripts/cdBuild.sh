7f25b5f06f25b6c363cfa73e3da71661d8a1d2a8
Patrick.Burls <patrick.burls@bskyb.com>
2020-06-04 17:46:24 +0100

    attmpt to copy junit reports into correct folder

--- integration/cd-scripts/cdBuild.sh
+++ integration/cd-scripts/cdBuild.sh
@@ -6,4 +6,8 @@ project_dir="${script_dir}/../"
 cd $project_dir
 
 # run the e2e integration tests
-../gradlew :integration:check
+../gradlew :integration:test
+
+# copy the test results to the correct location for the jenkins publisher
+# https://github.com/sky-uk/jenkins-tooling/blob/36d28ee4dae27a10d3fde208cb9b73b38136dbb9/examples/reference/cd.yml#L61
+cp -R build/test-results/test/ build/test-results/junit-reports/
