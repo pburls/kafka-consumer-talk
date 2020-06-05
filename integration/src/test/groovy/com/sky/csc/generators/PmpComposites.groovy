2628d7b5f9947b1f1ead1472e2e9e9538eebdfd2
Patrick.Burls <patrick.burls@bskyb.com>
2020-06-05 14:49:02 +0100

    add debugging for missing resources on class path

--- integration/src/test/groovy/com/sky/csc/generators/PmpComposites.groovy
+++ integration/src/test/groovy/com/sky/csc/generators/PmpComposites.groovy
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.sky.pmp.domain.AbstractCompositeEntity
 import com.sky.pmp.domain.CompositeParty
 import com.sky.pmp.domain.Source
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class PmpComposites {
+    static final Logger log = LoggerFactory.getLogger(PmpComposites.class);
     static final objectMapper = new ObjectMapper()
 
     static getEntityUuidFromComposite(AbstractCompositeEntity compositeEntity) {
@@ -22,10 +25,21 @@ class PmpComposites {
         def resourceFilePath = "/pmp-composites/${templateFileName}"
         def pmpResourceFileUrl = this.getClass().getResource(resourceFilePath)
         if (!pmpResourceFileUrl) {
+            logClassPathFiles()
             throw new RuntimeException("Unable to find resource file with path '${resourceFilePath}'")
         }
 
         return pmpResourceFileUrl.text
+    }
+
+    private static logClassPathFiles() {
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+
+        log.debug("Listing class path files:")
+        for(URL url: urls){
+            log.debug(url.getFile());
+        }
     }
 
     private static String replaceUuidTokens(String pmpCompositeJson) {
