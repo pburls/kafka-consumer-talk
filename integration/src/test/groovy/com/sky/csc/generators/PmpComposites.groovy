4855876cb7e7c0511e837d048d39451ad18c6be2
Patrick.Burls <patrick.burls@bskyb.com>
2020-06-05 13:49:06 +0100

    add runtime check to finding pmp composite template resource file

--- integration/src/test/groovy/com/sky/csc/generators/PmpComposites.groovy
+++ integration/src/test/groovy/com/sky/csc/generators/PmpComposites.groovy
@@ -7,9 +7,19 @@ class PmpComposites {
     static final objectMapper = new ObjectMapper()
 
     static CompositeParty generatePartyComposite() {
-        def pmpPartyCompositeJson = this.getClass().getResource('/pmp-composites/party.json').text
+        def pmpPartyCompositeJson = getPmpCompositeTemplateJson("party.json")
         pmpPartyCompositeJson = replaceUuidTokens(pmpPartyCompositeJson)
         return objectMapper.readValue(pmpPartyCompositeJson, CompositeParty.class)
+    }
+
+    static getPmpCompositeTemplateJson(String templateFileName) {
+        def resourceFilePath = "/pmp-composites/${templateFileName}"
+        def pmpResourceFileUrl = this.getClass().getResource(resourceFilePath)
+        if (!pmpResourceFileUrl) {
+            throw new RuntimeException("Unable to find resource with path '${resourceFilePath}'")
+        }
+
+        return pmpResourceFileUrl.text
     }
 
     static String replaceUuidTokens(String pmpCompositeJson) {
