e9e53c99f09c80832b64fb203c8d6d7c64359d3b
Patrick.Burls <patrick.burls@bskyb.com>
2020-06-05 18:03:54 +0100

    change to class loader for get resource

--- integration/src/test/groovy/com/sky/csc/generators/PmpComposites.groovy
+++ integration/src/test/groovy/com/sky/csc/generators/PmpComposites.groovy
@@ -22,25 +22,25 @@ class PmpComposites {
     }
 
     private static getPmpCompositeTemplateJson(String templateFileName) {
-        def resourceFilePath = "/pmp-composites/${templateFileName}"
-        def pmpResourceFileUrl = this.getClass().getResource(resourceFilePath)
+        def resourceFilePath = "pmp-composites/${templateFileName}"
+        def pmpResourceFileUrl = this.getClassLoader().getResource(resourceFilePath)
         if (!pmpResourceFileUrl) {
-            logClassPathFiles()
+            //logClassPathFiles()
             throw new RuntimeException("Unable to find resource file with path '${resourceFilePath}'")
         }
 
         return pmpResourceFileUrl.text
     }
 
-    private static logClassPathFiles() {
-        ClassLoader cl = ClassLoader.getSystemClassLoader();
-        URL[] urls = ((URLClassLoader)cl).getURLs();
-
-        System.out.println("Listing class path files:")
-        for(URL url: urls){
-            System.out.println(url.getFile());
-        }
-    }
+//    private static logClassPathFiles() {
+//        ClassLoader cl = ClassLoader.getSystemClassLoader();
+//        URL[] urls = ((URLClassLoader)cl).getURLs();
+//
+//        System.out.println("Listing class path files:")
+//        for(URL url: urls){
+//            System.out.println(url.getFile());
+//        }
+//    }
 
     private static String replaceUuidTokens(String pmpCompositeJson) {
         def tokenRegex = "(<new-uuid-(.)>)"
