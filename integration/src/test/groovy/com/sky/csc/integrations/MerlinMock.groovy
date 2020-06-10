e3faa93c7e13a0d0d55f2d03057f727f93cb018f
Patrick.Burls <patrick.burls@bskyb.com>
2020-06-10 16:31:03 +0100

    add log debug messages to merlin mock helper

--- integration/src/test/groovy/com/sky/csc/integrations/MerlinMock.groovy
+++ integration/src/test/groovy/com/sky/csc/integrations/MerlinMock.groovy
@@ -5,12 +5,16 @@ import com.sky.csc.metadata.ddi.DdiFragmentType
 import groovyx.net.http.HttpResponseException
 import groovyx.net.http.RESTClient
 import org.awaitility.Awaitility
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class MerlinMock {
+    static final Logger log = LoggerFactory.getLogger(MerlinMock.class);
     static final restClient = new RESTClient(Configuration.MerlinMockConfig.hostUrl)
 
     static Object getMerlinObject(DdiFragmentType fragmentType, String uuid) {
         // find a way to get any created merlin objects from the Merlin stub/mock
+        log.debug("Polling Merlin Mock for entity created with id '${uuid}'.")
         def merlinRequest = null
         Awaitility.await().ignoreExceptionsMatching(MerlinMock.&isNotFoundException).until { merlinRequest = getMerlinRequestForId(uuid).responseData }
         return merlinRequest
