3981ee8124eba49a22ab74df6bddaa4bd12ec524
Patrick.Burls <patrick.burls@bskyb.com>
2020-06-10 16:43:38 +0100

    change gradle test config to generate junit xml report to correct location for collection by jenkins

--- integration/cd-scripts/cdBuild.sh
+++ integration/cd-scripts/cdBuild.sh
@@ -7,7 +7,3 @@ cd $project_dir
 
 # run the e2e integration tests
 ../gradlew :integration:test
-
-# copy the test results to the correct location for the jenkins publisher
-# https://github.com/sky-uk/jenkins-tooling/blob/36d28ee4dae27a10d3fde208cb9b73b38136dbb9/examples/reference/cd.yml#L61
-cp -R build/test-results/test/ build/test-results/junit-reports/
