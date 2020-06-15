fc5e88c010b9e3760e8a7b603ea61a91da3c3ead
Patrick.Burls <patrick.burls@bskyb.com>
2020-06-15 17:17:14 +0100

    change DDI person test to done

--- integration/src/test/groovy/com/sky/csc/metadata/ddi/PersonSpec.groovy
+++ integration/src/test/groovy/com/sky/csc/metadata/ddi/PersonSpec.groovy
@@ -2,14 +2,12 @@ package com.sky.csc.metadata.ddi
 
 import com.sky.csc.categories.DDI
 import com.sky.csc.categories.Done
-import com.sky.csc.categories.WIP
 import com.sky.csc.generators.PmpComposites
 import com.sky.csc.integrations.CsaPersistedTopics
 import com.sky.csc.integrations.MerlinMock
 import com.sky.csc.integrations.PmpDdiOutboundTranslator
 import com.sky.csc.metadata.ddi.model.Person
 import org.junit.experimental.categories.Category
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import spock.lang.Specification
 
@@ -20,12 +18,12 @@ class PersonSpec extends Specification {
     static final log = LoggerFactory.getLogger(PersonSpec.class);
     static final validator = Validation.buildDefaultValidatorFactory().getValidator();
 
-    @Category(WIP)
+    @Category(Done)
     def "PMP Party composite to DDI PERSON fragment to Merlin Person object"() {
         given: "A PMP Party composite"
         def pmpPartyComposite = PmpComposites.generatePartyComposite()
         def entityUUID = PmpComposites.getEntityUuidFromComposite(pmpPartyComposite)
-        log.debug("PMP Party composite with PD Reference '${entityUUID}'")
+        log.debug("Created PMP Party composite with PD Reference '${entityUUID}'")
 
         and: "a TopicListener for the CSA DDI Person persisted topic"
         def personTopicListener = CsaPersistedTopics.createTopicListener(DdiFragmentType.Person)
@@ -44,30 +42,5 @@ class PersonSpec extends Specification {
         and: "a Merlin Person object should be found in the Merlin Mock"
         def merlinPersonObject = MerlinMock.getMerlinObject(DdiFragmentType.Person, entityUUID)
         merlinPersonObject
-    }
-
-    @Category(Done)
-    def "Example Done DDI PERSON fragment test"() {
-        given: "nothing"
-        def a = 1
-        def b = 1
-
-        when: "something happens"
-        def result = a + b
-
-        then: "pass"
-        result == 2
-    }
-
-    def "Example un-categorised DDI PERSON fragment test"() {
-        given: "nothing"
-        def a = 1
-        def b = 1
-
-        when: "something happens"
-        def result = a + b
-
-        then: "pass"
-        result == 2
     }
 }
